d90ee18d9e957de7c2cfbbe8a6687927505f13bc
tiago lopez <tiagovlopez@gmail.com>
2024-03-18 11:44:16 -0300

    dificultades agregadas, pero sin terminar

--- game.py
+++ game.py
@@ -7,23 +7,47 @@ words = ["python", "programación", "computadora", "código", "desarrollo",
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 
-# Número máximo de intentos permitidos
 max_attempts = 10
 
-# Lista para almacenar las letras adivinadas
-guessed_letters = []
-
 print("¡Bienvenido al juego de adivinanzas!")
-print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
-word_displayed = "_" * len(secret_word)
+print("""En que dificultad desea jugar
+      1-Facil
+      2-Media
+      3-Dificil""")
 
+difficulty = int(input("Ingrese dificultad: "))
+
+
+print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
+
+## diferentes dificultades
+word_displayed = ""
+## facil
+if difficulty == 1:
+   for element in secret_word:
+     if element in "aeiou":
+          word_displayed += element
+     else :
+        word_displayed += "_"
+else:
+   ## medio
+   if difficulty == 2:
+        word_displayed += secret_word[0] + "_" * (len(secret_word) - 2) + secret_word[-1]
+   else:
+      ##dificil
+     if difficulty == 3:
+         word_displayed = "_" * len(secret_word)
+        
+
+# Lista para almacenar las letras adivinadas
+guessed_letters = [word_displayed]
 
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 
 #uso un while en ves de un for con una variable fallos para contar los 10 fallos que tiene el usuario
-fallos = 0
-while fallos < 10 : 
+tries = 0
+while tries < 10 : 
 
  # Pedir al jugador que ingrese una letra
  letter = input("Ingresa una letra: ").lower()
@@ -42,7 +66,7 @@ while fallos < 10 :
     print("¡Bien hecho! La letra está en la palabra.")
  else:
     print("Lo siento, la letra no está en la palabra.")
-    fallos += 1
+    tries += 1
 
  # Mostrar la palabra parcialmente adivinada
  letters = []
@@ -50,7 +74,7 @@ while fallos < 10 :
     if letter in guessed_letters:
         letters.append(letter)
     else:
-        letters.append("_")
+     letters.append("_")
 
  word_displayed = "".join(letters)
  print(f"Palabra: {word_displayed}")
