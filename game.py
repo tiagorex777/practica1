87c7f5be75a50d174e78ac3b57b22353f321febb
tiago lopez <tiagovlopez@gmail.com>
2024-03-18 10:42:52 -0300

    el usuario para adivinar la palabra tendra 10 fallos, cambio en la linea 24

--- game.py
+++ game.py
@@ -17,10 +17,14 @@ print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
 
+
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
-for i in range(max_attempts):
- 
+
+#uso un while en ves de un for con una variable fallos para contar los 10 fallos que tiene el usuario
+fallos = 0
+while fallos < 10 : 
+
  # Pedir al jugador que ingrese una letra
  letter = input("Ingresa una letra: ").lower()
 
@@ -38,6 +42,7 @@ for i in range(max_attempts):
     print("¡Bien hecho! La letra está en la palabra.")
  else:
     print("Lo siento, la letra no está en la palabra.")
+    fallos += 1
 
  # Mostrar la palabra parcialmente adivinada
  letters = []
