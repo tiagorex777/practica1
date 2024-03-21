ef7dd05a23c9a7b561f04226c749c5a530fcd654
hola <hola@hola.com>
2024-03-21 08:42:09 -0300

    dificultades terminadas

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+ 
 import random
 
 # Lista de palabras posibles
@@ -10,12 +11,14 @@ secret_word = random.choice(words)
 max_attempts = 10
 
 print("¡Bienvenido al juego de adivinanzas!")
+
+#menu whit the diferrents dificulties
 print("""En que dificultad desea jugar
       1-Facil
-      2-Media
+      2-Media                                    
       3-Dificil""")
 
-difficulty = int(input("Ingrese dificultad: "))
+difficulty = int(input("Ingrese dificultad(1,2 o 3): "))
 
 
 
@@ -28,34 +31,38 @@ while difficulty < 1 or difficulty > 3:
 
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 
-## diferentes dificultades
+#diferrents dificulties
 word_displayed = ""
-## facil
+#easy
 if difficulty == 1:
    for element in secret_word:
      if element in "aeiou":
-       guessed_letters.append(element) ## guardo las letras dadas por el programa
+       guessed_letters.append(element)   #i keep all the letters the program give me
        word_displayed += element
      else:
        word_displayed += "_"
 else:
-   ## medio
+   #medium
     if difficulty == 2:
-      guessed_letters.append(secret_word[0])           
-      guessed_letters.append(secret_word[-1])
-      word_displayed += secret_word[0] + "_" * (len(secret_word) - 2) + secret_word[-1]
+     guessed_letters.append(secret_word[0])        
+     guessed_letters.append(secret_word[-1])
+     for elem in secret_word:   
+       if elem in guessed_letters:    #show all the ocurrencies of the first and last letter
+        word_displayed += elem
+       else:
+         word_displayed += "_"
     else: 
-      ##dificil
-      if difficulty == 3:
-          word_displayed = "_" * len(secret_word)
+     #hard
+     if difficulty == 3:
+       word_displayed = "_" * len(secret_word)
  
 
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 
-#uso un while en ves de un for con una variable fallos para contar los 10 fallos que tiene el usuario
+
 tries = 0
-while tries < 10 : 
+while tries < 10 :         #change the for whit a while,using a new variable that count the tries
 
  # Pedir al jugador que ingrese una letra
  letter = input("Ingresa una letra: ").lower()
@@ -65,13 +72,12 @@ while tries < 10 :
    if letter in secret_word:
     print("Ya has intentado con esa letra, es correcta. Intenta con otra.")
    else:
-     print("ya has intentado con este letras, es incorrecta. Intenta con otra")
-     tries += 1
+     print("ya has intentado con este letras, es incorrecta. Intenta con otra") 
+     tries += 1   #if te user put the same letter two times and it´s a worng letter, count as a try
    continue
 
  # Agregar la letra a la lista de letras adivinadas
- # corrijo si el usuario no pone nada
- if letter != "":
+ if letter != "":                                    #fixed if the users enter an empty letter(dont count as a try)
    guessed_letters.append(letter)
 
  # Verificar si la letra está en la palabra secreta
@@ -79,7 +85,7 @@ while tries < 10 :
       print("¡Bien hecho! La letra está en la palabra.")
    else:
       print("Lo siento, la letra no está en la palabra.")
-      tries += 1
+      tries += 1          
  else:
     print("caracter invalido, ingrese otro")
 
